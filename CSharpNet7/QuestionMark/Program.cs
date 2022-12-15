337df7b923b70ed3dd58e6dba02a2f03ed0b8c3a
huy770485@gmail.com <huy770485@gmail.com>
2022-12-15 14:07:28 +0700

    bài về null

--- CSharpNet7/QuestionMark/Program.cs
+++ CSharpNet7/QuestionMark/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+int number = 10;
+double money = 10.5;
+bool check = true;
+string str = "Huy";
+
+// trong C# thường có 2 dạng kiểu chính 
+// 1 value type (dạng số) không chứa được null, 2 là  reference type (chuỗi, đối tượng) cho phép null.
+string name = null;
+// code cũ 
+Nullable<bool> a = null;
+
+// code mới => nullable value type 
+int? b = null;
+bool? flag = null;
+
+// ?: => ternary operator
+Console.WriteLine(name != null ? true : false);
+// is not null (!=)
+// is (==)
+Console.WriteLine(name is not null ? true : false);
+// ? => null conditional operator
+// ? dùng sau đít tên biến, hay đối tượng
+// ? tương tự (!=), nếu cái gì đó khác null thì lấy cái vế sau
+
+int? length = name?.Length;
+
